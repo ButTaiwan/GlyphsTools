250c6a8e2ed057504d5341b0b4d3af493e09d605
But <buttaiwan@gmail.com>
2021-10-19 00:59:05 +0800

    新增擴大選取範圍腳本

---
+++ TaiwanKit/TaiwanTool/SelectFullOutline.py
@@ -0,0 +1,15 @@
+#MenuTitle: 擴大選取範圍到完整外框
+# -*- coding: utf-8 -*-
+#
+# (c) But Ko, 2021.
+# https://zi-hi.com/
+# https://github.com/ButTaiwan/GlyphsTools
+# https://www.facebook.com/groups/glyphszhtw
+#
+
+if Glyphs.font.selectedLayers is not None and len(Glyphs.font.selectedLayers) == 1:
+	layer = Glyphs.font.selectedLayers[0]
+	nodes = [obj for obj in layer.selection if type(obj)==GSNode]
+	for n in nodes:
+		path = n.parent
+		path.selected = True
